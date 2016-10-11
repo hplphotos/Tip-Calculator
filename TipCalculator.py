d5d3167fd3ab68ecd3b6734cd1241d30379a0ab2
hplphotos <hplphotos@gmail.com>
2016-10-10 20:42:00 -0400

    TipCalculator added

---
+++ TipCalculator.py
@@ -0,0 +1,18 @@
+# Calculate the amount people pay when splitting a restaurant bill
+#
+# Get input
+Total_bill=float(input("What is the total bill? "))
+How_Many_People=int(input("How many people will be splitting the bill? "))
+Tip_percent=float(input("What is the tip percentage? "))/100
+
+#Calculate results
+pre_tip_per_person=Total_bill/How_Many_People
+tip_per_person=pre_tip_per_person*Tip_percent
+each_person_share_incl_tip=pre_tip_per_person+tip_per_person
+total_bill_incl_tip=each_person_share_incl_tip*How_Many_People
+
+#Print results
+print("\nAmount per person before tip", "{:6.2f}".format(pre_tip_per_person))
+print("Amount per person including tip", "{:6.2f}".format(each_person_share_incl_tip))
+print("Total tip ", "{:5.2f}".format(How_Many_People*tip_per_person))
+print("Total bill including tip", "{:6.2f}".format(Total_bill+How_Many_People*tip_per_person))
